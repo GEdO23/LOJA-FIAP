4a851f2101acfbf4c7c2595a1d335c2ab5333e84
GEdO23 <rm99632@fiap.com.br>
2023-09-06 01:49:22 -0300

    Brincando com página de aparelhos

--- my-create-app/src/routes/Aparelhos/index.jsx
+++ my-create-app/src/routes/Aparelhos/index.jsx
@@ -1,7 +1,22 @@
 export default function Aparelhos() {
+
+  const listaAparelhos = [
+    {
+      id: 1,
+      nome: 'Ola Jeff'
+    },
+    {
+      id: 2,
+      nome: 'Ola mauricio'
+    },
+  ]
+
   return (
     <main>
-        <div>Aparelhos</div>
+      <h2>Aparelhos</h2>
+      {listaAparelhos.map(aparelho=> (
+        <li>{aparelho.nome}</li>
+      ))}
     </main>
   )
 }
