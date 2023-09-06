2d85893543a020c89fd3548b407b49aa5ca6e023
GEdO23 <rm99632@fiap.com.br>
2023-09-06 08:29:57 -0300

    Adcionando página de aparelhos com lista atualizad

---
+++ my-create-app/src/components/AparelhosData.jsx
@@ -0,0 +1,51 @@
+import teclado from '../img/teclado.jpg'
+import mouse from '../img/mouse.jpg'
+
+
+export const listaAparelhos = [
+    {
+      id: 0,
+      nome: 'Teclado RGB',
+      precOrig: 4500.00,
+      precDescont: 2500.00,
+      capacidade: 512,
+      resumo: "Semi Mecanico",
+      imgAlt: 'Teclado com teclas RGB',
+      imgSrc: teclado,
+    },
+    {
+      id: 1,
+      nome: 'Mouse mouse',
+      precOrig: 120.00,
+      precDescont: 69.99,
+      capacidade: 256,
+      resumo: 'Sem fio',
+      imgSrc: mouse,
+    },
+    {
+      id: 2,
+      nome: 'Celular bombastico Samsung Galaxy S23 Ultra',
+      precOrig: 5999.99,
+      precDescont: 3704.05,
+      capacidade: 256,
+      resumo: "Faça as melhores selfies à noite #WithGalaxy",
+      imgSrc: "#",
+    },
+    {
+      id: 3,
+      nome: 'Mesa digitalizadora bacana',
+      precOrig: 199.90,
+      precDescont: 179.90,
+      capacidade: 1,
+      resumo: "Mesa Digitalizadora Zinnia Momentum MT200, Resolucao 5080LPI, Niveis de Pressao 8192, Preto, ZNC-ZNMT200-BL01"
+    },
+    {
+      id: 4,
+      nome: 'Filtro de linha 5 tomadas preto WI297 Multilaser',
+      precOrig: 34.06,
+      precDescont: 24.06,
+      capacidade: 5,
+      resumo: "Fabricado com muito cuidado e atenção aos detalhes"
+    }
+];
+  
--- my-create-app/src/routes/Visualizar Aparelhos/index.jsx
+++ my-create-app/src/routes/Visualizar Aparelhos/index.jsx
@@ -1,7 +1,21 @@
-export default function VisualizarAparelhos() {
+import { useParams } from 'react-router-dom';
+import { listaAparelhos } from '../../components/AparelhosData'
+
+export default function VisualizarAparelho() {
+  const { id } = useParams();
+  const aparelho = listaAparelhos.find((ap) => ap.id.toString() === id);
+
+  if (!aparelho) {
+    return <p>Aparelho não encontrado.</p>;
+  }
+
   return (
-    <main>
-        <div>VisualizarAparelhos</div>
-    </main>
-  )
+    <div className="conteudo">
+      <h1>{aparelho.nome}</h1>
+      <p>Preço Original: R$ {aparelho.precOrig}</p>
+      <p>DESCONTO: R$ {aparelho.precDescont}</p>
+      <p>Capacidade: {aparelho.capacidade} GB</p>
+      <p>Descrição: {aparelho.resumo}</p>
+    </div>
+  );
 }
