789bdcadc2500c388cb7fd09bf3fe7dca5f33615
GEdO23 <rm99632@fiap.com.br>
2023-09-06 01:45:35 -0300

    Atualizando cabecalho v.1

--- my-create-app/src/components/Cabecalho.jsx
+++ my-create-app/src/components/Cabecalho.jsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom'
+import '../index.css'
+
 export default function Cabecalho() {
   return (
     <header>
-        <div>Cabecalho</div>
+
+      <h1 id='cabec-tit'>LOJA FIAP</h1>
+
+      <div className='cabec-menu'>
+        <Link to='/' className='cabec-link'>HOME</Link>
+        <Link to='/aparelhos' className='cabec-link'>APARELHOS</Link>
+      </div>
+
     </header>
   )
 }
